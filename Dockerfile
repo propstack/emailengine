968214bb9393bbcb9d290de1a1391ae074c7de69
pi3ter <pwiacek@valuelogic.one>
2022-07-27 18:25:00 +0200

    added nginx conf (more than 1 exposed port)

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM 728137396354.dkr.ecr.eu-west-1.amazonaws.com/s24-base-nodejs12
+FROM 728137396354.dkr.ecr.eu-west-1.amazonaws.com/s24-base-nodejs16
 
 ENV STACK_PATH=/emailengine
 
@@ -6,11 +6,14 @@ RUN yum install -y awscli jq && \
     yum clean all && \
     yum autoremove -y
 
+RUN amazon-linux-extras install nginx1 -y
+
 WORKDIR $STACK_PATH
 COPY . .
 
 RUN npm install --production
 
 EXPOSE 3000
+EXPOSE 80
 
 CMD ["sh", "/emailengine/docker/start-service.sh"]
--- docker/start-service.sh
+++ docker/start-service.sh
@@ -12,13 +12,15 @@ getSecretValueWithoutEnvPrefix() {
     jq -r ".Parameters[] | select(.Name == \"$1\") | .Value" secrets.json
 }
 
-aws ssm get-parameters --names \
-            "${ENV_PREFIX}EENGINE_REDIS" \
-        --region eu-west-1 --with-decryption > secrets.json
+#aws ssm get-parameters --names \
+#            "${ENV_PREFIX}EENGINE_REDIS" \
+#        --region eu-west-1 --with-decryption > secrets.json
 
-export EENGINE_REDIS=$(getSecretValue "EENGINE_REDIS")
+#todo: uncomment after tests
+#export EENGINE_REDIS=$(getSecretValue "EENGINE_REDIS")
+#rm secrets.json
 
-rm secrets.json
+mv ./deploy/nginx.conf /etc/nginx/nginx.conf
 
 echo "Starting Email Engine application"
-node /emailengine/server.js
+node /emailengine/server.js & nginx -c /etc/nginx/nginx.conf
