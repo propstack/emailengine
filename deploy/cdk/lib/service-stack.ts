dad474755f6a13a5bf54bc8d4f171e6bd3e025ff
pi3ter <pwiacek@valuelogic.one>
2022-07-29 11:33:40 +0200

    added datadog and set log level, removed unneeded file

--- deploy/cdk/lib/service-stack.ts
+++ deploy/cdk/lib/service-stack.ts
@@ -71,9 +71,8 @@ export class ServiceStack extends s24.Stack {
             unhealthyThresholdCount: 5,
             containerEnvironment: {
                 ENV_TYPE: props.stage,
-                // NODE_OPTIONS: "--require /usr/lib/node_modules/dd-trace/init",
-                EENGINE_PORT: "3000",
-                EENGINE_HOST: "0.0.0.0"
+                NODE_OPTIONS: "--require /usr/lib/node_modules/dd-trace/init",
+                EENGINE_LOG_LEVEL: "INFO"
             },
             role: new s24.InfinityServiceRole(
                 this, `InfinityServiceRole`, {
